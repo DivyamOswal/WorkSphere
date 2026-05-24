8a58c3a6669a6af0cb0a3e46e88b6fa43589ae8e
Divyam Mukesh Oswal <divyam9850400550@gmail.com>
2026-05-24 08:58:38 +0530

    cors login error solved

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://work-sphere-rose.vercel.app/login" 
+    "https://work-sphere-rose.vercel.app/" 
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
